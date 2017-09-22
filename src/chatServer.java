c1d5b95df7d5013ca3cdfbfab40caea47ccd8f4e
Juaquin <Noahdavidian@hotmail.dk>
2017-09-22 08:58:08 +0200

    Made some variables in the ChatServer class, a socket variable, socketServer variable, data in and out stream variable and a currently empty string variable

---
+++ src/chatServer.java
@@ -0,0 +1,15 @@
+import java.net.*;
+import java.io.*;
+
+
+public class chatServer {
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream streamIn = null;
+    private DataOutputStream streamOut = null;
+    private String name;
+
+
+
+}
+
