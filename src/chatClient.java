330c3ab7343effe01720a2f7ed0523d5a67917a7
Juaquin <Noahdavidian@hotmail.dk>
2017-09-22 11:31:36 +0200

    Made both Java classes needed for a Multithreaded server!

--- src/chatClient.java
+++ src/chatClient.java
@@ -1,6 +1,50 @@
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.Scanner;
 
+public class chatClient implements Runnable {
 
+    private Socket s;
 
-public class chatClient {
+    public chatClient(Socket s) throws SocketException,IOException {
+
+        this.s =s;
+
+    }
+    @Override
+    public void run() {
+        try {
+            try {
+                InputStream input = s.getInputStream();
+                OutputStream output = s.getOutputStream();
+
+                Scanner in = new Scanner(input);
+                PrintWriter out = new PrintWriter(output,true);
+                 out.println("Welcome!");
+
+                 boolean done = false;
+                 while(!done && in.hasNextLine()){
+                     String stream = in.nextLine();
+                     if(stream.equals("close")){
+                         done= true;
+
+                     }else {
+                         out.println(stream);
+                     }
+                 }
+
+            }
+            finally {
+                s.close();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/chatServer.java
+++ src/chatServer.java
@@ -1,13 +1,52 @@
 import java.net.*;
 import java.io.*;
+import java.net.Socket;
+import java.io.IOException;
+import java.net.ServerSocket;
+
 
 
 public class chatServer {
-    private Socket socket = null;
-    private ServerSocket server = null;
-    private DataInputStream streamIn = null;
-    private DataOutputStream streamOut = null;
-    private String name;
+    public static void main(String[]args) {
+        // ServerSocket being made:
+
+        ServerSocket ss;
+        try {
+            ss = new ServerSocket(8001);
+            System.out.println("Server is running...");
+            while(true) {
+                Socket incoming = ss.accept();
+
+                Runnable r = new chatClient(incoming);
+                Thread t = new Thread(r);
+                t.start();
+
+            }
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
